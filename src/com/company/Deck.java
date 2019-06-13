5ad721d388362f6eecc293bb093f3601396e850f
Pawel <pawel@kleist.pl>
2019-06-13 19:13:49 +0200

    Added v0.01

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -6,7 +6,17 @@ import java.util.Random;
 public class Deck {
 
     private ArrayList<Cards> deck = new ArrayList<Cards>();
-    private ArrayList<Cards> hand = new ArrayList<Cards>();
+    private ArrayList<Cards> handPlayer = new ArrayList<Cards>();
+    private ArrayList<Cards> handComputer = new ArrayList<Cards>();
+
+
+    public ArrayList<Cards> getHandPlayer() {
+        return handPlayer;
+    }
+
+    public ArrayList<Cards> getHandComputer() {
+        return handComputer;
+    }
 
     public Deck() {
         Random generator = new Random();
@@ -22,16 +32,31 @@ public class Deck {
 
         for(int i=1;i<=5;i++) {
             index1 = 1+ generator.nextInt(deck.size()-1);
-            hand.add(deck.get(index1));
+            handPlayer.add(deck.get(index1));
             deck.remove(index1);
-            System.out.println(hand.get(i-1));
+            System.out.println(handPlayer.get(i-1));
         }
 
-        System.out.println("Rest of the deck: ");
+        System.out.println("\nComputer's hand: \n");
+        for(int k=1;k<=5;k++) {
+            index2 = 1+ generator.nextInt(deck.size()-1);
+            handComputer.add(deck.get(index2));
+            deck.remove(index2);
+            System.out.println(handComputer.get(k-1));
 
-        for(int j=0;j<=46;j++) {
+        }
+
+        //public void check(ArrayList<Cards> setCard) {
+
+        //}
+
+        /*System.out.println("Rest of the deck: ");
+
+        for(int j=0;j<=deck.size()-1;j++) {
             System.out.println(deck.get(j));
         }
+        */
+
 
 
     }
